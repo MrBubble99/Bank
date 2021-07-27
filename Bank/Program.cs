dca8a5d1f48442295d38ba3ca9408a3e07938b31
Emilio Buchelt <bucheltemilio@gmail.com>
2021-07-27 13:22:46 -0300

    Version2.0

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -14,55 +14,43 @@ namespace Bank
 
         static void Main(string[] args)
         {
-           User Us = new User();
-           Transaction Trans = new Transaction();
-
+           
            List<Transaction> TransList = new List<Transaction>();
            List<User> UserList = new List<User>();
 
-
-
-            Us.Unsername = "Emilio";
-            Us.PassWort = "123";
-            Us.USD = 2500;
-            Us.BankNumber = 12345;
-            UserList.Add(Us);
-
-
-            Us.Unsername = "Valentina";
-            Us.PassWort = "456";
-            Us.USD = 2500;
-            Us.BankNumber = 345678;
-            UserList.Add(Us);
-
-
-            //Us.Unsername = "Amelia";
-            //Us.PassWort = "789";
-            //Us.USD = 2500;
-            //Us.BankNumber = 89101112;
-            //UserList.Add(Us);
+            bool logedIn = false;
+            User Us = new User();
+            Transaction Trans = new Transaction();
+            UserList.Add(new User { Unsername = "Master", PassWort = "123", USD = 0000, BankNumber = 57646099 });
+            UserList.Add(new User { Unsername = "Emilio", PassWort = "123", USD = 2500, BankNumber = 12345 });
+            UserList.Add(new User { Unsername = "Valentina", PassWort = "123", USD = 2500, BankNumber = 345678 });
 
             var rand = new Random();
             string actualUser = "";
             int actualUserAccount = 0;
-            bool logedIn = false;
+         
             int actualUserMony = 0;
             int actualUserId;
-            for (int y = 0; y < 4; y++)
+
+            try { 
+            for (int y = 0; y < 100; y++)
             {
+                    
+                    
 
                 Console.WriteLine("Do yo have a User? Answer with yes / no");
                 string answer = Console.ReadLine();
 
                 if (answer.ToLower().Equals("yes"))
                 {
-
+                        
+                        
 
                     Console.WriteLine("Introduce your Username");
                     string userName = Console.ReadLine();
                     Console.WriteLine("Introduce your Passwort");
                     string passwort = Console.ReadLine();
-
+                        //Check user list to verificate introduced pw and user
                     for (int i = 0; i < UserList.Count; i++)
                     {
                         if (userName == UserList[i].Unsername && passwort == UserList[i].PassWort)
@@ -73,46 +61,139 @@ namespace Bank
                             actualUserId = i;
 
                             logedIn = true;
-                            Console.WriteLine("your logedin");
+                            Console.WriteLine($"Hello {actualUser}");
                             Console.WriteLine($"your balance is: {UserList[i].USD}");
-                            Console.WriteLine("Do you want to make a transaction? Yes/No");
-                            string trans = Console.ReadLine();
-                            if (trans.ToLower() == "yes")
-                            {
-                                Console.WriteLine("Introduce the name from the benefited");
-                                Trans.RecivingUser = Console.ReadLine();
-                                Console.WriteLine("Introduce the Acount number from the benefited");
-                                Trans.AcountNoRU = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Introdce the amount you will transfer");
-                                Trans.TrasAmount = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Introdce the reference for the transfer");
-                                Trans.RefTrans = Console.ReadLine();
-                                Trans.AcountNoPU = actualUserAccount;
-                                Trans.PayingUser = actualUser;
-
-                                for (int x = 0; x < UserList.Count; x++)
+                            Console.WriteLine($"your Account Number is: {actualUserAccount}");
+                                //Acction to do in account
+                            Console.WriteLine("Do you want to make a transaction? Pres 1 / Do you want see the transaction historial press 2 / To logout press 3");
+                                if (actualUser.Equals("Master"))
                                 {
-                                    if (Trans.RecivingUser == UserList[x].Unsername && Trans.AcountNoRU == UserList[x].BankNumber && Trans.TrasAmount <= actualUserMony)
+                                    Console.WriteLine("Put mony on the account to transfere to user press 4 / See Total of mony in circulation press 5");
+                                    int MasterOption = Convert.ToInt32(Console.ReadLine());
+                                    if (MasterOption.Equals(4))
                                     {
-                                        UserList[actualUserId].USD = /*UserList[actualUserId].USD */- Trans.TrasAmount;
-                                        UserList[x].USD = /*UserList[x].USD*/ + Trans.TrasAmount;
+                                        Console.WriteLine("Introduce the name from the benefited");
+                                        Trans.RecivingUser = Console.ReadLine();
+                                        Console.WriteLine("Introduce the Acount number from the benefited");
+                                        Trans.AcountNoRU = Convert.ToInt32(Console.ReadLine());
+                                        Console.WriteLine("Introdce the amount you will transfer");
+                                        Trans.TrasAmount = Convert.ToInt32(Console.ReadLine());
+                                        Console.WriteLine("Introdce the reference for the transfer");
+                                        Trans.RefTrans = Console.ReadLine();
+                                        Trans.AcountNoPU = actualUserAccount;
+                                        Trans.PayingUser = actualUser;
                                         
 
-                                        TransList.Add(Trans);
-                                        Console.WriteLine("your transaction was done succesfuly");
-                                       
+                                        for (int x = 0; x < UserList.Count; x++)
+                                        {
+
+                                            if (Trans.RecivingUser == UserList[x].Unsername && Trans.AcountNoRU == UserList[x].BankNumber)
+                                            { 
+                                                UserList[x].USD = UserList[x].USD + Trans.TrasAmount;
+                                                UserList[0].USD = UserList[0].USD + 1;
+
+
+
+                                                TransList.Add(Trans);
+                                                Console.WriteLine("your transaction was done succesfuly");
+                                                Console.WriteLine($"your Balance is :  {UserList[actualUserId].USD}");
+                                                Console.WriteLine($"From : {TransList[0].AcountNoPU} To : {TransList[0].AcountNoRU}  amount: {TransList[0].TrasAmount} Reference: {TransList[0].RefTrans}");
+                                                Console.WriteLine($"Charge the person $ {Trans.TrasAmount + 1}");
+                                                break;
+                                            }
+                                           
+                                        }
+
+                                        
+                                    }
+                                    if (MasterOption.Equals(5))
+                                    {
+                                        int TotalMony = 0;
+                                        for (int z = 0; z < UserList.Count; z++)
+                                        {
+                                            TotalMony = TotalMony + UserList[z].USD;
+
+                                           
+                                              
+                                            }
+                                        Console.WriteLine(TotalMony);
+                                        break;
                                     }
                                 }
+                            string trans = Console.ReadLine();
+                               if (trans.Equals("1"))
+                                {
+                                   
+                                        Console.WriteLine("Introduce the name from the benefited");
+                                        Trans.RecivingUser = Console.ReadLine();
+                                        Console.WriteLine("Introduce the Acount number from the benefited");
+                                        Trans.AcountNoRU = Convert.ToInt32(Console.ReadLine());
+                                        Console.WriteLine("Introdce the amount you will transfer");
+                                        Trans.TrasAmount = Convert.ToInt32(Console.ReadLine());
+                                        Console.WriteLine("Introdce the reference for the transfer");
+                                        Trans.RefTrans = Console.ReadLine();
+                                        Trans.AcountNoPU = actualUserAccount;
+                                        Trans.PayingUser = actualUser;
+
+
+                                    for (int x = 0; x < UserList.Count; x++)
+                                    {
+                                        if (Trans.RecivingUser == UserList[x].Unsername && Trans.AcountNoRU == UserList[x].BankNumber && Trans.TrasAmount <= actualUserMony)
+                                        {
+                                            UserList[actualUserId].USD = UserList[actualUserId].USD - Trans.TrasAmount;
+                                            UserList[x].USD = UserList[x].USD + Trans.TrasAmount;
+                                            UserList[actualUserId].USD = UserList[actualUserId].USD - 1;
+                                            UserList[0].USD = UserList[0].USD + 1;
 
 
 
+                                            TransList.Add(Trans);
+                                            Console.WriteLine("your transaction was done succesfuly");
+                                            Console.WriteLine($"your Balance is :  {UserList[actualUserId].USD}");
+                                            Console.WriteLine($"From : {TransList[0].AcountNoPU} To : {TransList[0].AcountNoRU}  amount: {TransList[0].TrasAmount} Reference: {TransList[0].RefTrans}");
+                                            break;
+                                        }
+                                    }
+                                        
+                               }else if (trans.Equals("2")) 
+                                {
+                                    if (actualUser.Equals("Master"))
+                                    {
+                                        int TotalAmountInCirculation = 0;
+                                        for (int g = 0; g < TransList.Count; g++)
+                                        {
+                                            Console.WriteLine($"From : {TransList[g].AcountNoPU} To : {TransList[g].AcountNoRU}  amount: {TransList[g].TrasAmount} Reference: {TransList[g].RefTrans}");
+                                            TotalAmountInCirculation = + UserList[g].USD;
+                                        }
+                                        Console.WriteLine($"The total amount {TotalAmountInCirculation}");
+                                        break;
+                                    }
+                                    else 
+                                    {
+                                        for (int g = 0; g < TransList.Count; g++)
+                                        {
+                                            if (actualUserAccount.Equals(TransList[g].AcountNoPU) || actualUserAccount.Equals(TransList[g].AcountNoRU))
+                                            { Console.WriteLine($"From : {TransList[g].AcountNoPU} To : {TransList[g].AcountNoRU}  amount: {TransList[g].TrasAmount} Reference: {TransList[g].RefTrans}"); }
+                                            break;
+                                        }
+                                    }
+                                } else if (trans.Equals("3"))
+                                {
+                                    logedIn = false;
+                                    break;
+                                }       
+                                        
 
+                                    
 
-                            }
+                                       
+
+                                }
+                           
                         }
                     }
 
-                }
+                
                 else
                 {
                     Console.WriteLine("Create your User now !!!");
@@ -125,14 +206,20 @@ namespace Bank
                     Us.BankNumber = rand.Next();
                     UserList.Add(Us);
                     Console.WriteLine("Your user is created. Please Login");
+                    
 
-                }
+                    }
 
 
+            }
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
 
         }
 
+        
 
         }
 
